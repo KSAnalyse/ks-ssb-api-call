5d5711a33b7000bdf492cd67db7d74790538b62d
Hama Keli <hama.keli@ks.no>
2021-09-28 12:15:52 +0200

    Cleaned up App.java to just being the entrypoint.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,16 +1,5 @@
-import no.ks.fiks.ssbAPI.APIService.SsbApiCall;
-
-import java.io.IOException;
-
 public class App {
-    public static void main(String[] args) throws IOException {
-        test();
-    }
+    public static void main(String[] args) {
 
-    private static void test() throws IOException {
-        SsbApiCall test = new SsbApiCall("12367", "131", "104", "214", "231", "127");
-        test.metadataApiCall();
-        test.klassApiCall();
-        test.tableApiCall();
     }
 }
